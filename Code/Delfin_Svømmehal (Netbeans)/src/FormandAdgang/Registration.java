2524b6423770117e3a782217724a45612b182457
Svense <emil.svensmark@gmail.com>
2018-11-13 09:16:29 +0100

    Code added

--- Code/Delfin_Svømmehal (Netbeans)/src/FormandAdgang/Registration.java
+++ Code/Delfin_Svømmehal (Netbeans)/src/FormandAdgang/Registration.java
@@ -1,10 +1,12 @@
 
 package FormandAdgang;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Registration {
 
     ArrayList<Member> registreredMembers;
+    String membersFile = "C:\\members.txt";
     
     public void registerMember(Member member) {
         this.registreredMembers.add(member);
