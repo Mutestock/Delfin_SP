e32a9a03014daa1e68b58f06d966920310730d4f
Svense <emil.svensmark@gmail.com>
2018-11-13 09:11:33 +0100

    Code added

--- Code/Delfin_Svømmehal (Netbeans)/src/FormandAdgang/Registration.java
+++ Code/Delfin_Svømmehal (Netbeans)/src/FormandAdgang/Registration.java
@@ -1,8 +1,14 @@
 
 package FormandAdgang;
+import java.util.ArrayList;
 
 public class Registration {
 
+    ArrayList<Member> registreredMembers;
+    
+    public void registerMember(Member member) {
+        this.registreredMembers.add(member);
+    }
     
 }
 
