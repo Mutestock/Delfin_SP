fec788a4952cc806eb586e6f511b6ab7f8d9feeb
Emil-Skovbo <emilskovbo18@gmail.com>
2018-11-13 11:28:08 +0100

    local date

--- Code/Delfin_Svømmehal (Netbeans)/src/Kontingent/Kontingent.java
+++ Code/Delfin_Svømmehal (Netbeans)/src/Kontingent/Kontingent.java
@@ -5,10 +5,20 @@
  */
 package Kontingent;
 
-/**
- *
- * @author ahmed
- */
+import FormandAdgang.Member;
+import java.time.LocalDate;
+
+
+
 public class Kontingent {
+    
+    private LocalDate dayOfVisit = LocalDate.now();
+    private String description;
+
+    public Kontingent(LocalDate dayOfVisit,String description) {
+        this.dayOfVisit = dayOfVisit;
+        this.description = description;
+    }
+    
     
 }
