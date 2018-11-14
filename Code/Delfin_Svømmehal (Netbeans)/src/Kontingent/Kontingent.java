c36afbea3a50abcc9804579b459d951640765540
Mutestock <doublenegative@protonmail.com>
2018-11-14 12:03:12 +0100

    opaksd

--- Code/Delfin_Svømmehal (Netbeans)/src/Kontingent/Kontingent.java
+++ Code/Delfin_Svømmehal (Netbeans)/src/Kontingent/Kontingent.java
@@ -1,16 +1,94 @@
 package Kontingent;
+
 import FormandAdgang.Member;
+<<<<<<< HEAD
+import FormandAdgang.MemberAF;
+import java.time.LocalDate;
+=======
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+>>>>>>> 2b9680d0ed788407d7da6767afefd659c5d07349
 
 /**
  *
  * @author ahmed
  */
-
 public class Kontingent {
 
+    //The function of this class is for the kasserer(cashier) to manage the data of the subscribers.
+    //This includes using the basic information which is derived from the "FormandAdgang" package,
+    //as a means of calculating the fee of the subscriber.
     
+<<<<<<< HEAD
+//    private LocalDate dayOfVisit = LocalDate.now();
+//    private String description;
+ 
+    Member member;
+    private String kontingentFile;
+    private int subCatalogueYear;
+    private int subFee;
+
+    public Kontingent(Member member,int subCatalogueYear) {
+        this.subCatalogueYear = subCatalogueYear;
+        
+        if (member.isActivity()==true)
+        {
+            if (member.getAge()<=60)
+            {
+                subFee = 1200;
+            }
+            else if (member.getAge()<=18)
+            {
+                subFee = 1600;
+            }
+            else
+            {
+                subFee = 1000;
+            }
+        }
+        subFee= 500;  
+    }
+
+    //Currently working as a separate entity, not using the list of registered members.
+    
+    public int calcFee (Member member)
+    {
+        if (member.isActivity()==true)
+        {
+            if (member.getAge()<=60)
+            {
+                return 1200;
+            }
+            else if (member.getAge()<=18)
+            {
+                return 1600;
+            }
+            else
+            {
+                return 1000;
+            }
+        }
+        return 500;
+    }
+
+    public int getSubCatalogueYear() {
+        return subCatalogueYear;
+    }
+    
+  
+
+    public int getSubFee() {
+        return subFee;
+    }
+
+    public void setSubFee(int subFee) {
+        this.subFee = subFee;
+    }
+
+
+ 
+
+=======
     private LocalDateTime kontingentÅr;
     private String sum ;
   
@@ -32,4 +110,5 @@ public class Kontingent {
         this.Kontingent = hasPaid;
         
     }*/
+>>>>>>> 2b9680d0ed788407d7da6767afefd659c5d07349
 }
