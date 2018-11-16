a268d486d3c54c0416741cf75961c51b4f61f3ca
A O <ahmedothman8@hotmail.com>
2018-11-16 10:54:20 +0100

    test

--- Code/Delfin_Svømmehal (Netbeans)/src/Kontingent/Kontingent.java
+++ Code/Delfin_Svømmehal (Netbeans)/src/Kontingent/Kontingent.java
@@ -39,9 +39,9 @@ public class Kontingent {
 
     //Currently working as a separate entity, not using the list of registered members.
     //Note that using additional fields could improve the quality of the 60+ 25% rule.
-<<<<<<< HEAD
+
     public static int calcFee(Member member) {
-        if (member.isActivity() == true) {
+        if (member.isActive() == true) {
             if (member.getAge() >= 60) {
                 return 1200;
             } else if (member.getAge() >= 18) {
@@ -52,52 +52,6 @@ public class Kontingent {
         }
         return 500;
     } // ville være en god ide at sætte den i member
-
-    public static PrintWriter createFile(String kontingentFilePath) {
-
-=======
-    
-    
-    
-    
-    // ------------------------------------------
-    // ----------------To Henning----------------
-    // ------------------------------------------
-    // ------------------------------------------
-    /* createFile() bør være en metode som simpelthen bare kan lave en fil
-        navngivet efter en String parameter. Den metode bør sættes i den kommende
-        fil-håndterings-klasse.
-    */
-    public PrintWriter createFile(String FilePath) {
->>>>>>> 2cfeb695e65a84a082fe0b077901102514c95c2b
-        try {
-            File listOfNames = new File(FilePath);
-            PrintWriter infoWriter = new PrintWriter(new FileWriter(listOfNames));
-
-            return infoWriter;
-        } catch (IOException ex) {
-            System.out.println("IO Exception!");
-            System.exit(0);
-
-        }
-        return null;
-    } //lav en pakke som har createfile, så man har et sted med denne metode
-
-    // ------------------------------------------
-    // ----------------To Henning----------------
-    // ------------------------------------------
-    // ------------------------------------------
-    /* createSubscription() kan vi godt beholde her i en kontingentklasse
-        men jeg vil gerne have den simplificeret til det punkt hvor alt ang.
-        filer i denne metode foregår gennem andre metode fra fil klassen.
-    */
-    public void createSubscription(Member member, PrintWriter output) {
-
-        String feeConv = Integer.toString(member.calcFee());
-        String subYear = Integer.toString(member.getSubCatalogueYear());
-        output.println(member + feeConv + subYear);
-    }
-
     
     // ------------------------------------------
     // ----------------To Henning----------------
