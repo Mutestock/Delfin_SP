6c7401f9d2a5416ed92d6da37e53bb64a6889948
Emil-Skovbo <emilskovbo18@gmail.com>
2018-11-19 15:38:02 +0100

    ffuh

--- Code/Delfin_Svømmehal (Netbeans)/test/Controller/ControllerTest.java
+++ Code/Delfin_Svømmehal (Netbeans)/test/Controller/ControllerTest.java
@@ -5,6 +5,8 @@
  */
 package Controller;
 
+import FormandAdgang.Member;
+import FormandAdgang.Registration;
 import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -22,76 +24,22 @@ public class ControllerTest {
     public ControllerTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
+    @Test
+    public static void testcreateMember() {
+        
+        Member instance1 = new Member("test",7,"12345679",true,true);
+        Member instance2 = new Member("test",23,"12345679",true,true);
+        Member instance3 = new Member("test",72,"12345679",true,true);
+        Registration r = new Registration();
+        r.registerMember(instance1);
+        r.registerMember(instance2);
+        r.registerMember(instance3);
+        assertNull(r);
+        
     }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of createMember method, of class Controller.
-     */
-    @org.junit.Test
-    public void testCreateMember() {
-        System.out.println("createMember");
-        String name = "";
-        int age = 0;
-        String phone = "";
-        boolean activityForm = false;
-        boolean competetive = false;
-        Controller instance = new Controller();
-        instance.createMember(name, age, phone, activityForm, competetive);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of updateMember method, of class Controller.
-     */
-    @org.junit.Test
-    public void testUpdateMember() {
-        System.out.println("updateMember");
-        Controller instance = new Controller();
-        instance.updateMember();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getAllMembers method, of class Controller.
-     */
-    @org.junit.Test
-    public void testGetAllMembers() {
-        System.out.println("getAllMembers");
-        Controller instance = new Controller();
-        ArrayList<String> expResult = null;
-        ArrayList<String> result = instance.getAllMembers();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of deleteMember method, of class Controller.
-     */
-    @org.junit.Test
-    public void testDeleteMember() {
-        System.out.println("deleteMember");
-        String phone = "";
-        Controller instance = new Controller();
-        instance.deleteMember(phone);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    @Test
+    public static void testupdateMember() {
+        
     }
     
 }
